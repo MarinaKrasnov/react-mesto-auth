726b8d142561a65d306ff2ade1fc83f7ca7e9e99
MarinaKrasnov <93862896+MarinaKrasnov@users.noreply.github.com>
2023-06-14 13:58:25 +0300

    fix: deploy prepare

--- src/components/App.js
+++ src/components/App.js
@@ -1,206 +1,206 @@
-import React from 'react'
-import Header from './Header.js'
-import Main from './Main.js'
-import Footer from './Footer.js'
-import PopupWithForm from './PopupWithForm.js'
-import ImagePopup from './ImagePopup.js'
-import api from '../utils/api.js'
-import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
-import EditProfilePopup from './EditProfilePopup.js'
-import EditAvatarPopup from './EditAvatarPopup'
-import AddPlacePopup from './AddPlacePopup.js'
-import LogIn from './LogIn'
-import { Route, Switch, Link } from 'react-router-dom'
-import Register from './Register'
-import * as auth from './../utils/auth'
-import { useHistory } from 'react-router-dom'
-import ProtectedRoute from './ProtectedRoute'
-import { Redirect } from 'react-router-dom'
-import InfoTooltip from './InfoTooltip'
-function App () {
+import React from "react";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+import PopupWithForm from "./PopupWithForm.js";
+import ImagePopup from "./ImagePopup.js";
+import api from "../utils/api.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+import EditProfilePopup from "./EditProfilePopup.js";
+import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup.js";
+import LogIn from "./LogIn";
+import { Route, Switch, Link } from "react-router-dom";
+import Register from "./Register";
+import * as auth from "./../utils/auth";
+import { useHistory } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import { Redirect } from "react-router-dom";
+import InfoTooltip from "./InfoTooltip";
+function App() {
   // State constants
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(
-    false
-  )
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false)
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(
-    false
-  )
-  const [selectedCard, setSelectedCard] = React.useState(null)
-  const [currentUser, setCurrentUser] = React.useState({})
-  const [cards, setCards] = React.useState([])
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
-  const history = useHistory()
-  const [isInfoTooltipOpen, setInfoTooltip] = React.useState(false)
-  const [message, setMessage] = React.useState(false)
-  const [email, setEmail] = React.useState()
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState(null);
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [cards, setCards] = React.useState([]);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const history = useHistory();
+  const [isInfoTooltipOpen, setInfoTooltip] = React.useState(false);
+  const [message, setMessage] = React.useState(false);
+  const [email, setEmail] = React.useState();
   // Effects
   React.useEffect(() => {
     if (isLoggedIn) {
       Promise.all([api.getCards(), api.getProfileInfo()])
         .then(([cards, userData]) => {
-          setCards(cards)
-          setCurrentUser(userData)
+          setCards(cards);
+          setCurrentUser(userData);
         })
-        .catch(err => {
-          console.log(`Request for data from server is failed.${err}`)
-        })
+        .catch((err) => {
+          console.log(`Request for data from server is failed.${err}`);
+        });
     }
-  }, [isLoggedIn])
+  }, [isLoggedIn]);
   React.useEffect(() => {
     const checkToken = () => {
-      const jwt = localStorage.getItem('jwt')
+      const jwt = localStorage.getItem("jwt");
       if (jwt) {
-        auth.checkToken(jwt).then(response => {
-          setEmail(response.data.email)
-          setIsLoggedIn(true)
-          history.push('/main')
-        })
+        auth.checkToken(jwt).then((response) => {
+          setEmail(response.data.email);
+          setIsLoggedIn(true);
+          history.push("/main");
+        });
       }
-    }
-    checkToken()
-  }, [])
+    };
+    checkToken();
+  }, [history]);
   //Handlers
   const handleEditAvatarClick = () => {
-    setIsEditAvatarPopupOpen(true)
-  }
+    setIsEditAvatarPopupOpen(true);
+  };
   const handleEditProfileClick = () => {
-    setIsEditProfilePopupOpen(true)
-  }
+    setIsEditProfilePopupOpen(true);
+  };
   const handleAddPlaceClick = () => {
-    setIsAddPlacePopupOpen(true)
-  }
+    setIsAddPlacePopupOpen(true);
+  };
   const closeAllPopups = () => {
-    setIsEditProfilePopupOpen(false)
-    setIsAddPlacePopupOpen(false)
-    setIsEditAvatarPopupOpen(false)
-    setSelectedCard(null)
-    setInfoTooltip(false)
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setSelectedCard(null);
+    setInfoTooltip(false);
+  };
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
   }
 
-  function handleCardClick (card) {
-    setSelectedCard(card)
-  }
-
-  function handleEditProfileChange ({ name, about }) {
+  function handleEditProfileChange({ name, about }) {
     api
       .editProfileInfo({ name, about })
-      .then(userData => {
-        setCurrentUser(userData)
-        closeAllPopups()
+      .then((userData) => {
+        setCurrentUser(userData);
+        closeAllPopups();
       })
-      .catch(err => {
-        console.log(`Request for data from server is failed.${err}`)
-      })
+      .catch((err) => {
+        console.log(`Request for data from server is failed.${err}`);
+      });
   }
 
-  function handleEditAvatar (avatar) {
+  function handleEditAvatar(avatar) {
     api
       .changeAvatar(avatar)
-      .then(data => {
-        setCurrentUser(data)
-        closeAllPopups()
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups();
       })
-      .catch(err => {
-        console.log(`Request for data from server is failed.${err}`)
-      })
+      .catch((err) => {
+        console.log(`Request for data from server is failed.${err}`);
+      });
   }
 
-  function handleCardLike (card) {
+  function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(i => i._id === currentUser._id)
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
       .changeLikeCardStatus(card._id, isLiked)
-      .then(newCard => {
-        setCards(state => state.map(c => (c._id === card._id ? newCard : c)))
+      .then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
       })
-      .catch(err => {
-        console.log(`Request for data from server is failed.${err}`)
-      })
+      .catch((err) => {
+        console.log(`Request for data from server is failed.${err}`);
+      });
   }
 
-  function handleCardDelete (card) {
+  function handleCardDelete(card) {
     api
       .deleteCard(card._id)
       .then(() => {
-        setCards(cards =>
-          cards.filter(item => {
-            return item._id !== card._id
+        setCards((cards) =>
+          cards.filter((item) => {
+            return item._id !== card._id;
           })
-        )
+        );
       })
-      .catch(err => {
-        console.log(`Request for data from server is failed.${err}`)
-      })
+      .catch((err) => {
+        console.log(`Request for data from server is failed.${err}`);
+      });
   }
 
-  function handleAddPlaceSubmit (newCard) {
+  function handleAddPlaceSubmit(newCard) {
     api
       .postCard(newCard)
-      .then(newCard => {
-        setCards([newCard, ...cards])
-        closeAllPopups()
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
       })
-      .catch(err => {
-        console.log(`Request for data from server is failed.${err}`)
-      })
+      .catch((err) => {
+        console.log(`Request for data from server is failed.${err}`);
+      });
   }
   const handleRegister = (password, email) => {
     auth
       .register(password, email)
-      .then(response => {
+      .then((response) => {
         if (response.data) {
-          setMessage(true)
-          setInfoTooltip(true)
+          setMessage(true);
+          setInfoTooltip(true);
           React.setTimeOut(() => {
-            history.push('/sign-in')
-          }, 3000)
+            history.push("/sign-in");
+          }, 3000);
         } else {
-          setMessage(false)
-          setInfoTooltip(true)
+          setMessage(false);
+          setInfoTooltip(true);
         }
       })
-      .then(res => {
+      .then((res) => {
         if (!res.data) {
-          setMessage(false)
-          setInfoTooltip(true)
+          setMessage(false);
+          setInfoTooltip(true);
         }
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   const handleLogin = (email, password) => {
-    auth.login(email, password).then(response => {
+    auth.login(email, password).then((response) => {
       if (response) {
-        localStorage.setItem('jwt', response.token)
-        setIsLoggedIn(true)
-        setEmail(email)
-        history.push('/main')
+        localStorage.setItem("jwt", response.token);
+        setIsLoggedIn(true);
+        setEmail(email);
+        history.push("/main");
       } else {
-        setMessage(false)
-        setInfoTooltip(true)
+        setMessage(false);
+        setInfoTooltip(true);
       }
-    })
-  }
+    });
+  };
 
   const handleSignOut = () => {
-    localStorage.removeItem('jwt')
-    setIsLoggedIn(false)
-    setEmail('')
-    history.push('/sign-in')
-  }
+    localStorage.removeItem("jwt");
+    setIsLoggedIn(false);
+    setEmail("");
+    history.push("/sign-in");
+  };
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <Switch>
-        <ProtectedRoute exact path='/main' isLoggedIn={isLoggedIn}>
-          <Header className={'header_type_loggedin'}>
-            <div className='header__email-container'>
-              <p className='header__email'>{email}</p>
+        <ProtectedRoute exact path="/main" isLoggedIn={isLoggedIn}>
+          <Header className={"header_type_loggedin"}>
+            <div className="header__email-container">
+              <p className="header__email">{email}</p>
               <Link
-                to='sign-in'
-                className='header__link link header__link_type_loggedin'
+                to="sign-in"
+                className="header__link link header__link_type_loggedin"
                 onClick={handleSignOut}
               >
                 Выйти
@@ -234,28 +234,28 @@ function App () {
             handleSubmit={handleAddPlaceSubmit}
           />
           <PopupWithForm
-            name={'ausure'}
-            title='Вы уверены?'
+            name={"ausure"}
+            title="Вы уверены?"
             onClose={closeAllPopups}
           >
-            <input type='hidden' name='id' defaultValue='' />
+            <input type="hidden" name="id" defaultValue="" />
 
             <button
-              type='submit'
-              className='popup__submit popup__submit_size_s'
-              aria-label='Кнопка согласия'
+              type="submit"
+              className="popup__submit popup__submit_size_s"
+              aria-label="Кнопка согласия"
             >
               Да
             </button>
           </PopupWithForm>
         </ProtectedRoute>
-        <Route path='/sign-up'>
-          <Header className={'header_type_login'}>
-            <Link to='sign-in' className='link'>
+        <Route path="/sign-up">
+          <Header className={"header_type_login"}>
+            <Link to="sign-in" className="link">
               Войти
             </Link>
           </Header>
-          <div className='form__container'>
+          <div className="form__container">
             <Register onRegister={handleRegister} />
           </div>
           <InfoTooltip
@@ -264,13 +264,13 @@ function App () {
             isOpen={isInfoTooltipOpen}
           />
         </Route>
-        <Route path='/sign-in'>
-          <Header className={'header_type_login'}>
-            <Link to='sign-up' className='header__link link'>
+        <Route path="/sign-in">
+          <Header className={"header_type_login"}>
+            <Link to="sign-up" className="header__link link">
               Регистрация
             </Link>
           </Header>
-          <div className='form__container'>
+          <div className="form__container">
             <LogIn onLogin={handleLogin} />
           </div>
           <InfoTooltip
@@ -279,12 +279,12 @@ function App () {
             isOpen={isInfoTooltipOpen}
           />
         </Route>
-        <Route exact path='/'>
-          {isLoggedIn ? <Redirect to='/main' /> : <Redirect to='/sign-in' />}
+        <Route exact path="/">
+          {isLoggedIn ? <Redirect to="/main" /> : <Redirect to="/sign-in" />}
         </Route>
       </Switch>
     </CurrentUserContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,89 +1,90 @@
 class Api {
-  constructor (url, headers) {
-    this._url = url
-    this._headers = headers
+  constructor(url, headers) {
+    this._url = url;
+    this._headers = headers;
   }
-  getCards () {
+  getCards() {
     return this._makeRequest(
       fetch(`${this._url}/cards`, {
-        method: 'GET',
-        headers: this._headers
+        method: "GET",
+        headers: this._headers,
       })
-    )
+    );
   }
-  _makeRequest (promise) {
-    return promise.then(res => {
+  _makeRequest(promise) {
+    return promise.then((res) => {
       if (res.ok) {
-        return res.json()
+        return res.json();
       }
-      throw 'Request failed'
-    })
+      // eslint-disable-next-line no-throw-literal
+      throw "Request failed";
+    });
   }
-  postCard ({ name, url }) {
+  postCard({ name, url }) {
     const promise = fetch(`${this._url}/cards`, {
-      method: 'POST',
+      method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name: name,
-        link: url
-      })
-    })
-    return this._makeRequest(promise)
+        link: url,
+      }),
+    });
+    return this._makeRequest(promise);
   }
-  deleteCard (id) {
+  deleteCard(id) {
     const promise = fetch(`${this._url}/cards/${id}`, {
-      method: 'DELETE',
-      headers: this._headers
-    })
-    return this._makeRequest(promise)
+      method: "DELETE",
+      headers: this._headers,
+    });
+    return this._makeRequest(promise);
   }
-  getProfileInfo () {
+  getProfileInfo() {
     return this._makeRequest(
       fetch(`${this._url}/users/me`, {
-        method: 'GET',
-        headers: this._headers
-      }).catch(err => alert(`Request failed ${err.status}`))
-    )
+        method: "GET",
+        headers: this._headers,
+      }).catch((err) => alert(`Request failed ${err.status}`))
+    );
   }
-  editProfileInfo ({ name, about }) {
-    console.log({ name, about })
+  editProfileInfo({ name, about }) {
+    console.log({ name, about });
     return this._makeRequest(
       fetch(`${this._url}/users/me`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: this._headers,
         body: JSON.stringify({
           name,
-          about
-        })
+          about,
+        }),
       })
-    )
+    );
   }
 
-  changeAvatar (avatar) {
+  changeAvatar(avatar) {
     return this._makeRequest(
       fetch(`${this._url}/users/me/avatar`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: this._headers,
         body: JSON.stringify({
-          avatar
-        })
+          avatar,
+        }),
       })
-    )
+    );
   }
-  changeLikeCardStatus (id, isLiked) {
+  changeLikeCardStatus(id, isLiked) {
     return this._makeRequest(
       fetch(`${this._url}/cards/${id}/likes`, {
-        method: isLiked ? 'DELETE' : 'PUT',
-        headers: this._headers
+        method: isLiked ? "DELETE" : "PUT",
+        headers: this._headers,
       })
-    )
+    );
   }
 }
 
-const api = new Api('https://mesto.nomoreparties.co/v1/cohort-38', {
-  Accept: 'application/json',
-  'Content-Type': 'application/json; charset=utf-8',
-  authorization: 'c5a7c514-ca8f-4b82-95f7-7b25ec57dd45'
-})
+const api = new Api("https://mesto.nomoreparties.co/v1/cohort-38", {
+  Accept: "application/json",
+  "Content-Type": "application/json; charset=utf-8",
+  authorization: "c5a7c514-ca8f-4b82-95f7-7b25ec57dd45",
+});
 
-export default api
+export default api;
